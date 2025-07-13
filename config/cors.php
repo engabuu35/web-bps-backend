fb16e7778f5b8044e559c51abda5bf7f2fb5ceca
engabuu35 <arbudibudibudi@gmail.com>
2025-07-13 12:07:39 +0700

    ubah cors.php saran jidan

--- config/cors.php
+++ config/cors.php
@@ -1,11 +1,17 @@
+
 <?php
 return [
-'paths' => ['api/*', 'sanctum/csrf-cookie'],
-'allowed_methods' => ['*'],
-'allowed_origins' => ['http://localhost:5173', 'https://web-bps-frontend-chi.vercel.app', 'https://web-bps-frontend-d662yvr5r-arif-budimans-projects-43063944.vercel.app'],
-'allowed_origins_patterns' => [],
-'allowed_headers' => ['*'],
-'exposed_headers' => [],
-'max_age' => 0,
-'supports_credentials' => true,
+    'paths' => ['api/*'],
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'http://localhost:5173','https://web-bps-frontend-chi.vercel.app'
+    ],
+
+    'allowed_origins_patterns' => ['https://web-bps-frontend-chi.vercel.app'],
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => true,
 ];
