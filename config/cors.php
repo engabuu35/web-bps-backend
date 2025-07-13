9174117ff07e792677d07ff6d4b392f783f510e1
engabuu35 <arbudibudibudi@gmail.com>
2025-07-13 12:01:45 +0700

    edit cors tambah link

--- config/cors.php
+++ config/cors.php
@@ -2,7 +2,7 @@
 return [
 'paths' => ['api/*', 'sanctum/csrf-cookie'],
 'allowed_methods' => ['*'],
-'allowed_origins' => ['http://localhost:5173', 'https://web-bps-frontend-chi.vercel.app'],
+'allowed_origins' => ['http://localhost:5173', 'https://web-bps-frontend-chi.vercel.app', 'https://web-bps-frontend-d662yvr5r-arif-budimans-projects-43063944.vercel.app'],
 'allowed_origins_patterns' => [],
 'allowed_headers' => ['*'],
 'exposed_headers' => [],
